cf9b89a4a6623d9c4c018d80e6a31c9f70ca9792
Sabitoxd <145153398+Sabitoxd@users.noreply.github.com>
2023-09-16 10:34:03 +0300

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,12 @@
+local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+
+local MyButton = LeftGroupBox:AddButton({
+    Text = 'Rejoin',
+    Func = function()
+local TeleportService = game:GetService("TeleportService")
+TeleportService:Teleport(game.PlaceId, LocalPlayer)
+    end,
+    DoubleClick = false,
+    Tooltip = 'This is the main button'
+})
