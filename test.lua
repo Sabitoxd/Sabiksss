76877ba15bf9924d2a4c10a3bfe0088eaab2baa5
Sabitoxd <145153398+Sabitoxd@users.noreply.github.com>
2023-09-16 10:41:14 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,22 @@
 local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 
+--окно
+local Window = Library:CreateWindow({
+    Title = 'SL2 AimBot',
+    Center = true,
+    AutoShow = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.2
+})
+
+--табы
+local Tabs = {
+    Main = Window:AddTab('Main'),
+}
+
+--бокс группа
+local LeftGroupBox = Window.Tabs.Main:AddLeftGroupbox('rejoin')
 local MyButton = LeftGroupBox:AddButton({
     Text = 'Rejoin',
     Func = function()
@@ -10,3 +26,10 @@ TeleportService:Teleport(game.PlaceId, LocalPlayer)
     DoubleClick = false,
     Tooltip = 'This is the main button'
 })
+--UI Settings
+local LeftGroupBox2 = Tabs.Main:AddLeftGroupbox('UI Settings')
+--выклбчить гуи
+LeftGroupBox2:AddButton('Unload', function() Library:Unload() end)
+--бинд менюшки
+LeftGroupBox2:AddLabel('Menu keybind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+Library.ToggleKeybind = Options.MenuKeybind```
